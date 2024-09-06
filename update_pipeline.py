1182deb1c6dd7f0c8cd233f6138ecf3bc9591b4c
Zloaq <tomotomoya0210@gmail.com>
2024-09-06 23:34:49 +0900

    minor_change13

--- update_pipeline.py
+++ update_pipeline.py
@@ -57,8 +57,6 @@ if __name__ == "__main__":
     saved_dir_names = save_directory_names(param_file)
 
     shebang = save_shebang(path_program)
-    print(path_program)
-    print(f'shebang={shebang}')
     comm = os.path.join(dir_of_program, 'ch_shebang.sh')
     git_pull()
     subprocess.run([comm, shebang], stdout=subprocess.DEVNULL)
