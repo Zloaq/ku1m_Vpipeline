3206c0473a22f7c0fa0f0e8b091ffdeaa4911120
Zloaq <tomotomoya0210@gmail.com>
2024-11-18 02:47:08 +0900

    minor_change

--- update_pipeline.py
+++ update_pipeline.py
@@ -33,11 +33,19 @@ class save_param():
                 continue
 
             items = ''
-            if varr[1].startswith(('\'', '\"', '(')):
+            if varr[1].startswith(('\'', '\"')):
                 items = f'{varr[1][0]}\\'
                 for index, item in enumerate(varr[1:]):
                     if item.endswith(('\'', '\"', ')')):
                         items = items + item[:-1] + f'\\{item[-1]}' + item[-1]
+                        varr[1] = items
+                        break
+                    items = items + item + ' '
+
+            if varr[1].startswith(('(', '[')):
+                for index, item in enumerate(varr[1:]):
+                    if item.endswith(('\'', '\"', ')')):
+                        items = items + item
                         varr[1] = items
                         break
                     items = items + item + ' '
